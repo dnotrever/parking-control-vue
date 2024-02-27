64c028f5cea4aa3512ddf40d9ef547b3eb27fe37
dnotrever <notrevedr@gmail.com>
2024-02-27 23:47:22 +0000

    creates the static login page

---
+++ parking-frontend/src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-96 mx-auto">
+        <div class="main">
+            <div class="p-12 bg-white border border-gray-200 rounded-lg">
+                <form class="space-y-6 flex flex-col items-center">
+                    <h2 class="mb-2 text-2xl">Access</h2>
+                    <div>
+                        <label class="ms-1">Email</label><br>
+                        <input type="email" placeholder="Insert here the email address" class="w-full mt-2 py-2 px-5 border border-gray-200 rounded-lg">
+                    </div>
+                    <div>
+                        <label class="ms-1">Password</label><br>
+                        <input type="password" placeholder="Insert here the password" class="w-full mt-2 py-2 px-5 border border-gray-200 rounded-lg">
+                    </div>
+                    <div>
+                        <button class="w-full mt-2 py-2 px-6 bg-purple-600 text-white rounded-lg">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
