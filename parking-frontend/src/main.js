f3c2e31f667d84a036d7ab9b773496148d21a3b8
dnotrever <notrevedr@gmail.com>
2024-02-27 01:00:50 +0000

    executes npm install and configures the axios

--- parking-frontend/src/main.js
+++ parking-frontend/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import './assets/main.css'
 
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
 const app = createApp(App)
 
-app.use(router)
+app.use(router, axios)
 
 app.mount('#app')
